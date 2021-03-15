16a0f5aed640246bb102bc3a94adec3e34166036
jtourvieille <jtourvieille@gmail.com>
2021-03-15 10:55:11 +0100

    feat: add home layout

--- Client/src/pages/Home/Home.js
+++ Client/src/pages/Home/Home.js
@@ -5,27 +5,33 @@ import './Home.scss';
 
 export const HomeComponent = ({ title, subtitle }) => (
   <div className="home container">
-    <h1 className="af-title--content">{title}</h1>
-    <h2>{subtitle}</h2>
-    <p>
-      Donec sollicitudin molestie malesuada. Donec sollicitudin molestie
-      malesuada. Donec rutrum congue leo eget malesuada. Vivamus suscipit tortor
-      eget felis porttitor volutpat. Sed porttitor lectus nibh. Curabitur arcu
-      erat, accumsan id imperdiet et, porttitor at sem. Vivamus magna justo,
-      lacinia eget consectetur sed, convallis at tellus. Proin eget tortor
-      risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Pellentesque in ipsum id orci porta dapibus.
-    </p>
-
-    <p>
-      Sed porttitor lectus nibh. Curabitur non nulla sit amet nisl tempus
-      convallis quis ac lectus. Mauris blandit aliquet elit, eget tincidunt nibh
-      pulvinar a. Sed porttitor lectus nibh. Quisque velit nisi, pretium ut
-      lacinia in, elementum id enim. Vivamus magna justo, lacinia eget
-      consectetur sed, convallis at tellus. Nulla quis lorem ut libero malesuada
-      feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-      suscipit tortor eget felis porttitor volutpat. Sed porttitor lectus nibh.
-    </p>
+    <h1 className="af-title--content">Accueil</h1>
+    <table className="af-table">
+      <thead>
+        <tr>
+          <th>i1</th>
+          <th>i2</th>
+          <th>i3</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <input type="text" id="id1" name="id1" required />
+          </td>
+          <td>
+            <input type="text" id="id2" name="id2" required />
+          </td>
+          <td>
+            <input type="text" id="id3" name="id3" required />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <button>Calculer</button>
+ 
+    <h2>Résultats</h2>
+    <p>Le coût total est de : xx.xx€ / mois</p>
   </div>
 );
 
