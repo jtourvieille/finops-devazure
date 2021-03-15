a05aedb603560d95febf971514c8bf10e44f0ce6
jtourvieille <jtourvieille@gmail.com>
2021-03-15 13:34:56 +0100

    feat: add projection

--- Client/src/pages/Home/Home.js
+++ Client/src/pages/Home/Home.js
@@ -7,16 +7,42 @@ export const HomeComponent = () => {
   const [i1Number, seti1Number] = useState(0);
   const [i2Number, seti2Number] = useState(0);
   const [i3Number, seti3Number] = useState(0);
-  const [result, setResult] = useState(0);
+  const [i1NumberProjection, seti1NumberProjection] = useState(0);
+  const [i2NumberProjection, seti2NumberProjection] = useState(0);
+  const [i3NumberProjection, seti3NumberProjection] = useState(0);
+  const [resultActuel, setResultActuel] = useState(0);
+  const [resultProjection, setResultProjection] = useState(0);
+  const [resultDiff, setResultDiff] = useState(0);
 
   const calculHandler = () => {
     const service = new CalculatorService();
-    setResult(
+    setResultActuel(
       service.calculateCost([
         { I1: i1Number },
         { I2: i2Number },
         { I3: i3Number },
       ])
+    );
+
+    setResultProjection(
+      service.calculateCost([
+        { I1: i1NumberProjection },
+        { I2: i2NumberProjection },
+        { I3: i3NumberProjection },
+      ])
+    );
+
+    setResultDiff(
+      service.calculateCost([
+        { I1: i1Number },
+        { I2: i2Number },
+        { I3: i3Number },
+      ]) -
+        service.calculateCost([
+          { I1: i1NumberProjection },
+          { I2: i2NumberProjection },
+          { I3: i3NumberProjection },
+        ])
     );
   };
 
@@ -62,27 +88,63 @@ export const HomeComponent = () => {
           <Table.Body>
             <Table.Tr>
               <Table.Td>
+                <label>Actuel :</label>
                 <Text
                   id="i1-nb"
                   name="i1-nb"
                   value={i1Number}
                   onChange={({ value }) => seti1Number(parseInt(value) || 0)}
                 />
-              </Table.Td>
-              <Table.Td>
+                <br />
+                <br />
+                <label>Projection :</label>
                 <Text
-                  id="i1-nb"
-                  name="i1-nb"
-                  value={i2Number}
-                  onChange={({ value }) => seti2Number(parseInt(value) || 0)}
+                  id="i1-nbP"
+                  name="i1-nbP"
+                  value={i1NumberProjection}
+                  onChange={({ value }) =>
+                    seti1NumberProjection(parseInt(value) || 0)
+                  }
                 />
               </Table.Td>
               <Table.Td>
+                <label>Actuel :</label>
                 <Text
-                  id="i1-nb"
-                  name="i1-nb"
+                  id="i2-nb"
+                  name="i2-nb"
+                  value={i2Number}
+                  onChange={({ value }) => seti2Number(parseInt(value) || 0)}
+                />
+                <br />
+                <br />
+                <label>Projection :</label>
+                <Text
+                  id="i2-nbP"
+                  name="i2-nbP"
+                  value={i2NumberProjection}
+                  onChange={({ value }) =>
+                    seti2NumberProjection(parseInt(value) || 0)
+                  }
+                />
+              </Table.Td>
+              <Table.Td>
+                <label>Actuel :</label>
+                <Text
+                  id="i3-nb"
+                  name="i3-nb"
                   value={i3Number}
                   onChange={({ value }) => seti3Number(parseInt(value) || 0)}
+                />
+                <br />
+                <br />
+                <label>Projection :</label>
+                <Text
+                  id="i3-nbP"
+                  name="i3-nbP"
+                  value={i3NumberProjection}
+                  onChange={({ value }) =>
+                    seti3NumberProjection(parseInt(value) || 0)
+                  }
                 />
               </Table.Td>
             </Table.Tr>
@@ -98,7 +160,12 @@ export const HomeComponent = () => {
         <br />
         <br />
         <h2>Résultat</h2>
-        <p>Le coût total est de : {result}€ / mois</p>
+        <p>Le coût actuel est de : {resultActuel}€ / mois</p>
+        <p>Le coût de la projection est de : {resultProjection}€ / mois</p>
+        <p>
+          La différence de coût entre le service plan actuel et le service pla
+          projetté est de : {resultDiff}€ / mois
+        </p>
         <br />
         <br /> <br />
         <br />
