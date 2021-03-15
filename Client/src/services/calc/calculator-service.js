686f9c7d03222c5ec4483f1d76e178bcf7529d81
Remi JACQUART <remi.jacquart@axa.fr>
2021-03-15 15:46:31 +0100

    feat(calc): calculate auto scale working days cost

--- Client/src/services/calc/calculator-service.js
+++ Client/src/services/calc/calculator-service.js
@@ -50,7 +50,33 @@ export class CalculatorService {
 
       const instanceCost = this.findInstanceCost(instanceLabel);
 
+      // 1 jour sur une instance
+      // 6 jours en mode normal
+      // 7 jours sur une semaine
       cost = ((6 * quantity + 1) * instanceCost) / 7;
+    }
+
+    return Math.round(cost * 100) / 100;
+  }
+
+  calculateAutoScaleWorkingHoursCost(instanceToCost) {
+    let instanceLabel;
+    let quantity;
+
+    let cost = 0;
+
+    if (instanceToCost) {
+      for (const [key, value] of Object.entries(instanceToCost)) {
+        instanceLabel = key;
+        quantity = value;
+      }
+
+      const instanceCost = this.findInstanceCost(instanceLabel);
+
+      // 50 heures sur une instance
+      // 118 heures en mode normal
+      // 168: nombres d'heures totales sur une semaine
+      cost = ((50 + 118 * quantity) * instanceCost) / 168;
     }
 
     return Math.round(cost * 100) / 100;
